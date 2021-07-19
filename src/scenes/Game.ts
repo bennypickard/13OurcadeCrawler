71fdc0d0e8e0c68d450b50aa652f01992e9615fc
bennypickard <bennypickard@gmail.com>
2021-07-19 16:22:20 -0700

    WallCollide Graphic
    Added a graphic tool that highlights which blocks are colliding.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -22,8 +22,19 @@ export default class Game extends Phaser.Scene
         const tileset = map.addTilesetImage('dungeon', 'tiles',  )
 
         map.createStaticLayer('Ground', tileset)
-        map.createStaticLayer("Walls", tileset )
+        const wallsLayer = map.createStaticLayer("Walls", tileset )
 
+        wallsLayer.setCollisionByProperty({collides: true});
+
+
+
+        const debugGraphics = this.add.graphics().setAlpha(0.7)
+        wallsLayer.renderDebug(debugGraphics,{
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+
+        })
 
 
     }
